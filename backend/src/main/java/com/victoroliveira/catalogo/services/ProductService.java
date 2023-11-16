4e00c04d382307d0ab27f3b3d2666a07dab413d4
Victor Oliveira <victor.oliveira20@icloud.com>
2023-11-16 18:01:51 -0300

    Refactor Pageable

--- backend/src/main/java/com/victoroliveira/catalogo/services/ProductService.java
+++ backend/src/main/java/com/victoroliveira/catalogo/services/ProductService.java
@@ -3,7 +3,7 @@ package com.victoroliveira.catalogo.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -29,8 +29,8 @@ public class ProductService {
 	private CategoryRepository categoryRepository;
 	
 	@Transactional(readOnly = true)
-	public Page<ProductDTO> findAllPaged(PageRequest pageRequest){		
-		Page<Product> list = repository.findAll(pageRequest);		
+	public Page<ProductDTO> findAllPaged(Pageable pageable){		
+		Page<Product> list = repository.findAll(pageable);		
 		return list.map(x -> new ProductDTO(x));
 	}
 	
