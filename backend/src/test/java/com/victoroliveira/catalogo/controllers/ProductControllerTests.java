fdf07ccdc70a3b5309fb632e6654b2f54cd7e028
Victor Oliveira <victor.oliveira20@icloud.com>
2023-12-13 17:48:34 -0300

    ProductControllerTests - findById(), finByIdThrow() implemented

--- backend/src/test/java/com/victoroliveira/catalogo/controllers/ProductControllerTests.java
+++ backend/src/test/java/com/victoroliveira/catalogo/controllers/ProductControllerTests.java
@@ -3,6 +3,7 @@ package com.victoroliveira.catalogo.controllers;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.List;
@@ -13,10 +14,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.PageImpl;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.victoroliveira.catalogo.dto.ProductDTO;
 import com.victoroliveira.catalogo.services.ProductService;
+import com.victoroliveira.catalogo.services.exceptions.ResourceNotFoundException;
 import com.victoroliveira.catalogo.tests.factory.Factory;
 
 @WebMvcTest(ProductController.class)
@@ -30,21 +33,43 @@ public class ProductControllerTests {
 	
 	private ProductDTO productDTO;
 	private PageImpl<ProductDTO> page;
+	private Long existingId;
+	private Long nonExistingId;
 	
 	@BeforeEach
 	void setUp() throws Exception {
+		
+		existingId = 1L;
+		nonExistingId = 2L;
 		
 		productDTO = Factory.createProductDTO();
 		
 		page = new PageImpl<>(List.of(productDTO));
 		
 		when(service.findAllPaged(any())).thenReturn(page);
+		when(service.findById(existingId)).thenReturn(productDTO);
+		when(service.findById(nonExistingId)).thenThrow(ResourceNotFoundException.class);
+		
 	}
 	
 	@Test
 	public void findAllShouldReturnPage() throws Exception {
-		mockMvc.perform(get("/products")).andExpect(status().isOk());
+		mockMvc.perform(get("/products").accept(MediaType.APPLICATION_JSON))
+		.andExpect(status().isOk());
 		
+	}
+	
+	@Test
+	public void findByIdShouldReturnProductDTOWhenExistingId() throws Exception {
+		mockMvc.perform(get("/products/{id}", existingId).accept(MediaType.APPLICATION_JSON))
+		.andExpect(status().isOk())
+		.andExpect(jsonPath("$.id").exists());
+	}
+	
+	@Test
+	public void findByIdShouldReturnNotFoundWhenNonExistingId() throws Exception {
+		mockMvc.perform(get("/products/{id}", nonExistingId).accept(MediaType.APPLICATION_JSON))
+		.andExpect(status().isNotFound());
 	}
 
 }
