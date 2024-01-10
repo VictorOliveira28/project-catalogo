f8026e0717e08c15624326e6e5a01d53bc94c2d4
Victor Oliveira <victor.oliveira20@icloud.com>
2024-01-10 15:17:08 -0300

    Custom Validation response

--- backend/src/main/java/com/victoroliveira/catalogo/controllers/exceptions/ControllerExceptionHandler.java
+++ backend/src/main/java/com/victoroliveira/catalogo/controllers/exceptions/ControllerExceptionHandler.java
@@ -4,6 +4,8 @@ import java.time.Instant;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -38,4 +40,22 @@ public class ControllerExceptionHandler {
 		err.setPath(request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 		}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ValidationError> methodArgumentNotValidException(MethodArgumentNotValidException e, HttpServletRequest request){
+		HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+		ValidationError err = new ValidationError();
+		err.setTimestamp(Instant.now());
+		err.setStatus(status.value());
+		err.setError("Method Argument Not Valid Exception");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+		
+		for(FieldError f : e.getBindingResult().getFieldErrors()) {
+			err.addError(f.getField(), f.getDefaultMessage());
+		}
+		
+		
+		return ResponseEntity.status(status).body(err);
+	}
 }
